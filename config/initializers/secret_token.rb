451fded9ee5fea10131427860a86183e5268e40c
Greg <Rcfan0902@aol.com>
2012-08-27 14:50:17 -0400

    First Website Push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exchange::Application.config.secret_token = '0e02a7ff03e7abcb7ae1e2e3091a5e7211d3e7cd4b1473b0cc84aa4f564a41c30e6168cb8da1c5f4b26089191796d9afefcec02a3a79c0ffdfa5ece50b7f645d'
